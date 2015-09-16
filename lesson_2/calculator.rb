fef91e7dbade3a9c9a74bbf22df8734e19273de6
ToniRib <tonimarierib@gmail.com>
2015-09-16 11:59:03 -0600

    Updates calculator.rb in lesson 2 to make the if statement return the result

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -17,14 +17,14 @@ puts "1) add 2) subtract 3) multiply 4) divide"
 operator = gets.chomp
 
 # perform the operation on the two numbers
-if operator == '1'
-  result = number1.to_i + number2.to_i
+result = if operator == '1'
+  number1.to_i + number2.to_i
 elsif operator == '2'
-  result = number1.to_i - number2.to_i
+  number1.to_i - number2.to_i
 elsif operator == '3'
-  result = number1.to_i * number2.to_i
+  number1.to_i * number2.to_i
 else
-  result = number1.to_f / number2.to_f
+  number1.to_f / number2.to_f
 end
 
 # output the result
