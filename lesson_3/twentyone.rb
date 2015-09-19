da45a238ef47326c72a8d14fe07404c38a0d8a04
ToniRib <tonimarierib@gmail.com>
2015-09-19 12:27:11 -0600

    twentyone: Removes cards from deck when they are dealt. Also displays the dealer and player hands.

--- lesson_3/twentyone.rb
+++ lesson_3/twentyone.rb
@@ -2,14 +2,49 @@
 
 require 'pry'
 
-SUITS = %w(h d c s)
+SUITS = %w(Hearts Diamonds Clubs Spades)
 NUM_CARDS = (2..10)
-NAME_CARDS = %w(j q k a)
+NAME_CARDS = %w(Jack Queen King Ace)
 PLAYERS = %w(dealer player)
+
+def prompt(message)
+  puts "\n=> #{message}"
+end
 
 def initialize_deck
   values = NUM_CARDS.to_a.map { |num| num.to_s }.push(NAME_CARDS).flatten
   values.product(SUITS)
 end
 
-initialize_deck
+def deal_initial_cards(deck)
+  cards = deck.sample(2)
+end
+
+def remove_cards_from_deck(deck, cards)
+  deck.reject! { |card| cards.include?(card) }
+end
+
+def display_one_card_of_dealer(hand)
+  puts "Dealer has: #{hand.sample[0]} and unknown card"
+end
+
+def display_player_hand(hand)
+  puts "You have: #{hand[0][0]} and #{hand[1][0]}"
+end
+
+puts "----------------------"
+puts "Welcome to Twenty One!"
+puts "----------------------"
+puts "\nDealing initial cards..."
+
+deck = initialize_deck
+player_hand = deal_initial_cards(deck)
+remove_cards_from_deck(deck, player_hand)
+dealer_hand = deal_initial_cards(deck)
+remove_cards_from_deck(deck, dealer_hand)
+
+display_one_card_of_dealer(dealer_hand)
+display_player_hand(player_hand)
+
+prompt "What would you like to do?"
+answer = gets.chomp.downcase
