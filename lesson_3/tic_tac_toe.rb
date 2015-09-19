394ddaf3051643b810713948b0837ab13844f1a4
ToniRib <tonimarierib@gmail.com>
2015-09-19 09:36:02 -0600

    Lots of updates to tic-tac-toe, should have broken this into multiple commits

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -14,6 +14,8 @@ WINNING_SET = [
   [3, 5, 7]
 ]
 MARKER_LOCATIONS = (1..9)
+PLAYERS = %w(user computer)
+MARKERS = %w(X O)
 
 def initialize_board
   new_board = {}
@@ -42,19 +44,19 @@ def prompt(message)
 end
 
 def valid_marker?(char)
-  char == 'X' || char == 'O'
+  MARKERS.include?(char)
 end
 
 def display_marker(char)
   case char
-  when 'X'
-    prompt "You chose 'X', so the computer will be 'O'"
-  when 'O'
-    prompt "You chose 'O', so the computer will be 'X'"
+  when MARKERS[0]
+    prompt "You chose #{MARKERS[0]}, so the computer will be #{MARKERS[1]}"
+  when MARKERS[1]
+    prompt "You chose #{MARKERS[1]}, so the computer will be #{MARKERS[0]}"
   end
 end
 
-def player_places_marker(board, marker)
+def player_places_marker!(board, marker)
   location = ''
   loop do
     prompt "Where would you like to place your marker? #{joinor(empty_positions(board))}"
@@ -68,13 +70,17 @@ def player_places_marker(board, marker)
   board.store(location.to_i, marker)
 end
 
-def computer_places_marker(board, marker)
-  location = ''
-  loop do
-    location = MARKER_LOCATIONS.to_a.sample
-    break if valid_location?(location, board)
-  end
+def computer_places_marker!(board, marker)
+  location = empty_positions(board).sample
   board.store(location.to_i, marker)
+end
+
+def place_piece!(board, cur_player, cur_marker)
+  if cur_player == 'user'
+    player_places_marker!(board, cur_marker)
+  else
+    computer_places_marker!(board, cur_marker)
+  end
 end
 
 def valid_location?(loc, board)
@@ -95,26 +101,47 @@ def win?(board, marker)
   combos.any? { |combo| WINNING_SET.include?(combo) }
 end
 
-def player_turn(board, marker)
-  player_places_marker(board, marker)
-  display_board(board)
-end
-
-def computer_turn(board, marker)
-  puts "The computer is thinking..."
-  sleep(1)
-  computer_places_marker(board, marker)
-  puts "The computer has chosen!"
-  display_board(board)
-end
-
 def display_tie_message
   puts "Looks like it's a tie this time."
 end
 
+def display_win_message(cur_player)
+  case cur_player
+  when 'user'
+    puts "You won this round!"
+  when 'computer'
+    puts "The computer beat you!"
+  end
+end
+
 def joinor(array, delim=', ', word='or')
   array[-1] = "#{word} #{array.last}" if array.size > 1
-  array.join(delim)
+  if array.size < 3
+    array.join(' ')
+  else
+    array.join(delim)
+  end
+end
+
+def determine_starting_player
+  PLAYERS.sample
+end
+
+def alternate_player(player)
+  player == PLAYERS[0] ? PLAYERS[1] : PLAYERS[0]
+end
+
+def opposite_marker(marker)
+  marker == MARKERS[0] ? MARKERS[1] : MARKERS[0]
+end
+
+def determine_starting_marker(cur_player, user, computer)
+  case cur_player
+  when 'user'
+    user
+  when 'computer'
+    computer
+  end
 end
 
 loop do # main game loop
@@ -122,7 +149,7 @@ loop do # main game loop
   puts "-----------------------"
 
   marker = ''
-  prompt "Would you like to be X or O?"
+  prompt "Would you like to be #{joinor(MARKERS)}?"
   loop do
     marker = gets.chomp.upcase
     if valid_marker?(marker)
@@ -133,27 +160,23 @@ loop do # main game loop
     end
   end
 
-  computer_marker = (marker == 'O') ? 'X' : 'O'
+  computer_marker = determine_computer_marker(marker)
 
   board = initialize_board
-  display_board(board)
+
+  current_player = determine_starting_player
+  current_marker = determine_starting_marker(current_player, marker, computer_marker)
 
   loop do # main turn loop
-    player_turn(board, marker)
-    if win?(board, marker)
-      puts "You win!!"
+    display_board(board)
+    place_piece!(board, current_player, current_marker)
+    current_player = alternate_player(current_player)
+    current_marker = alternate_marker(current_marker)
+    if win?(board, current_marker)
+      display_win_message(current_player)
       break
     elsif board_full?(board)
-      puts display_tie_message
-      break
-    end
-
-    computer_turn(board, computer_marker)
-    if win?(board, computer_marker)
-      puts "The computer beat you!!"
-      break
-    elsif board_full?(board)
-      puts display_tie_message
+      display_tie_message
       break
     end
   end
